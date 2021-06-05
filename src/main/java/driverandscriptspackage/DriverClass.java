beb254d40a87472ab5c89aa5fb6ca68ee43871c5
JAffa097 <jpgpgma666@gmail.com>
2021-06-06 00:59:10 +0530

    Build issues fixed

--- src/main/java/driverandscriptspackage/DriverClass.java
+++ src/main/java/driverandscriptspackage/DriverClass.java
@@ -7,8 +7,7 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.testng.annotations.Test;
+
 
 public class DriverClass {
 	private ThreadLocal<ChromeDriver> driver= new ThreadLocal();
